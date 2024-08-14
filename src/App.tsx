24f4d345f593f5c4ff1ee60ae20fcffb11455686
20nc040 <169446253+20nc040@users.noreply.github.com>
2024-08-14 10:40:30 +0900

    remove import React

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { GlobalStyles } from "@mui/material";
 import { useForm, SubmitHandler } from "react-hook-form"
 
